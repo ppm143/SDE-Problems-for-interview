ff7307ba2e89a4a4b542a4082450f6b6a7dda421
Parth Patel <65417976+ppm143@users.noreply.github.com>
2025-07-20 10:17:35 +0530

    Time: 97 ms (27.15%), Space: 76.3 MB (46.02%) - LeetHub

---
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    struct compare {
+        bool operator()(vector<int>& p, vector<int>& q) {
+            return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1];
+        }
+    };
+
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<vector<int>, vector<vector<int>>, compare> minQ;
+        for (vector<int>& point : points) {
+            minQ.push(point);
+            if (minQ.size() > k) {
+                minQ.pop();
+            }
+        }
+
+        vector<vector<int>> ans;
+        while (!minQ.empty()) {
+            ans.push_back(minQ.top());
+            minQ.pop();
+        }
+        return ans;
+    }
+};
