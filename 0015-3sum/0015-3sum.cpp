2cfe18c2f15c21975a92c0d58d7b11b64a07e809
Parth Patel <65417976+ppm143@users.noreply.github.com>
2025-08-30 11:51:51 +0530

    Time: 40 ms (91.82%), Space: 29.5 MB (37.2%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,25 +5,28 @@ public:
         vector<vector<int>> ans;
         int n = nums.size() - 1;
         for (int i = 0; i < nums.size() - 2; i++) {
+            if (nums[i] > 0)
+                break;
+            // to avoid duplicate
+            if (i > 0 and nums[i] == nums[i - 1])
+                continue;
             solveTwoSum(i, n, nums, ans);
-            // to avoid duplicate 
-            while (i + 1 < n and nums[i] == nums[i + 1])
-                i++;
         }
         return ans;
     }
 
-    void solveTwoSum(int idx, int e, vector<int>& nums, vector<vector<int>>& ans) {
+    void solveTwoSum(int idx, int e, vector<int>& nums,
+                     vector<vector<int>>& ans) {
         int s = idx + 1;
         while (s < e) {
             int sum = nums[s] + nums[e];
             if (sum == -nums[idx]) {
                 ans.push_back({nums[idx], nums[s], nums[e]});
-                // to avoid duplicate 
-                while (s < e and nums[s] == ans.back()[1])
+                s++;
+                e--;
+                // to avoid duplicate
+                while (s < e and nums[s] == nums[s - 1])
                     s++;
-                while (s < e and nums[e] == ans.back()[2])
-                    e--;
             } else if (sum > -nums[idx])
                 e--;
             else
